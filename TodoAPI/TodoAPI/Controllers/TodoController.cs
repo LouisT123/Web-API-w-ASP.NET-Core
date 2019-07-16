c64afb9ce70f34bb93785de77afbda1b2d45d2d1
Louis Taing <louistaing@Anands-MBP.guest.corp.microsoft.com>
2019-07-16 11:56:47 -0700

    Added a database context

---
+++ TodoAPI/TodoAPI/Controllers/TodoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using TodoApi.Models;
+using System.Threading.Tasks;
+
+namespace TodoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+
+            if (_context.TodoItems.Count() == 0)
+            {
+                //Create a new TodoItem if collection is empty,
+                // which mean you can't delete all TodoItems.
+                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+                _context.SaveChanges();
+            }
+        }
+    }
+}
